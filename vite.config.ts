6ca15f2422d573959c94ed8a5977c4f1ff5c9956
Shahad Uddin <126606838+shahaduddin@users.noreply.github.com>
2026-01-30 17:09:06 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +9,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        pynum: 'PyNum/index.html'
+        main: resolve('index.html'),
+        pynum: resolve('PyNum/index.html')
       },
     },
   },
