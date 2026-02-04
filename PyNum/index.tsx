400d8aca4ce03d3da87af0a5dc21ad881359a782
shahad-uddin <scientistphysics01@gmail.com>
2026-02-04 11:24:23 +0600

    Updated file

--- PyNum/index.tsx
+++ PyNum/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -10,6 +11,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Add the basename prop here */}
+    <BrowserRouter basename="/PyNum">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
